59d3aee25256ab62ca5c18e23b863e59edf4a344
jyun261 <90105419+jyun261@users.noreply.github.com>
2022-05-20 09:21:20 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os/exec"
 )
 
 func main() {
 	cmd := exec.Command("git", "commit", "-m", `"testing commits in go"`)
 	cmd.Run()
+	fmt.Print("Showing how to do shit.")
 }
